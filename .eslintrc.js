2db24df3b5320235c5a681e171759ae5aa7502ef
horike37 <horike37@gmail.com>
2019-02-01 12:04:58 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  extends: 'prettier',
+  plugins: ['import', 'prettier'],
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  rules: {
+    'array-bracket-spacing': [
+      'error',
+      'never',
+      {
+        objectsInArrays: false,
+        arraysInArrays: false
+      }
+    ],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    'func-names': 'off',
+    'no-use-before-define': 'off',
+    'prefer-destructuring': 'off',
+    'no-console': 'error',
+    'no-shadow': 'error',
+    'no-undef': 'error',
+    'object-curly-newline': 'off',
+    'no-unused-vars': 'error',
+    'semi': 'off',
+    'object-shorthand': 'off',
+    'prettier/prettier': 'error',
+    'prefer-const': 'error'
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Serverless Apigateway Service proxy
+The Serverless Framewrok plugin to support AWS service proxy integration of API Gateway
