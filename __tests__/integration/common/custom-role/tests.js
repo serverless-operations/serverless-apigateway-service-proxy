da0cc353e9bc37e07dc8fa5713f575727b7ec5ca
erezrokah <erezrokah@users.noreply.github.com>
2019-09-24 12:10:01 +0300

    feat: add support for specifying custom roles

---
+++ __tests__/integration/common/custom-role/tests.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const expect = require('chai').expect
+const fetch = require('node-fetch')
+const {
+  deployWithRandomStage,
+  removeService,
+  getS3Object,
+  deleteS3Object
+} = require('../../../utils')
+
+describe('Proxy With Custom Role Integration Test', () => {
+  let endpoint
+  let stage
+  let bucket
+  const config = '__tests__/integration/common/custom-role/service/serverless.yml'
+  const key = 'my-test-object.json'
+
+  beforeAll(async () => {
+    const result = await deployWithRandomStage(config)
+
+    stage = result.stage
+    endpoint = result.endpoint
+    bucket = result.outputs.S3BucketName
+  })
+
+  afterAll(async () => {
+    await deleteS3Object(bucket, key)
+    removeService(stage, config)
+  })
+
+  it('should get correct response from sqs proxy endpoint with default role', async () => {
+    const testEndpoint = `${endpoint}/sqs`
+
+    const response = await fetch(testEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: 'testtest' })
+    })
+    expect(response.headers.get('access-control-allow-origin')).to.deep.equal('*')
+    expect(response.status).to.be.equal(200)
+    const body = await response.json()
+    expect(body.SendMessageResponse.SendMessageResult).to.have.own.property(
+      'MD5OfMessageAttributes'
+    )
+    expect(body.SendMessageResponse.SendMessageResult).to.have.own.property('MD5OfMessageBody')
+    expect(body.SendMessageResponse.SendMessageResult).to.have.own.property('MessageId')
+    expect(body.SendMessageResponse.SendMessageResult).to.have.own.property('SequenceNumber')
+    expect(body.SendMessageResponse.ResponseMetadata).to.have.own.property('RequestId')
+  })
+
+  it('should get correct response from s3 proxy endpoint with custom role', async () => {
+    const testEndpoint = `${endpoint}/s3/${key}`
+
+    const response = await fetch(testEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: 'test' })
+    })
+    expect(response.headers.get('access-control-allow-origin')).to.deep.equal('*')
+    expect(response.status).to.be.equal(200)
+
+    const uploadedObject = await getS3Object(bucket, key)
+    expect(uploadedObject.toString()).to.equal(JSON.stringify({ message: 'test' }))
+  })
+})
